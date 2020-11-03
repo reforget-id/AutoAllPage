7f2d46eaf22c8a0a36be33acc231185f74c11c95
fahriza.fahmi99@gmail.com <fahriza.fahmi99@gmail.com>
2020-11-03 22:52:14 +0700

    Update version 1.0.0
    Add support for websites :
    tribunnews, merdeka, suara, matamata, sindonews, inews, grid.id

--- script/autoallpage.user.js
+++ script/autoallpage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Auto All Page
-// @version       0.0.2
+// @version       1.0.0
 // @author        reforget-id
 // @namespace     autoallpage
 // @icon          https://img.icons8.com/cotton/2x/overview-pages-1.png
@@ -10,32 +10,116 @@
 // @updateURL     https://raw.githubusercontent.com/reforget-id/AutoAllPage/main/script/autoallpage.user.js 
 // @include       http*://*.detik.com/*
 // @include       http*://*.kompas.com/*
+// @include       http*://*.tribunnews.com/*
+// @include       http*://*.merdeka.com/*
+// @include       http*://*.suara.com/*
+// @include       http*://*.matamata.com/*
+// @include       http*://*.sindonews.com/*
+// @include       http*://*.inews.id/*
+// @include       http*://*.grid.id/*
+// @include       http*://*.bolasport.com/*
+// @include       http*://*.motorplus-online.com/*
+// @include       http*://*.gridoto.com/*
 // @run-at        document-start
 // ==/UserScript==
 
 
-function detikRedirect() {
-    let url = window.location.href
-    let regex = /(?<=^.+\.detik\.com\/.+\/.+\/.+)(\?(tag_from|(?!single).*)|(?<=[a-zA-Z])|\/\d?)$/
-    //let regex = /(?<=^.+\.detik\.com\/.+\/.+\/.+)((?<!\?single=\d)|\?tag_from.*|\/\d?)$/
-    let matcher = url.match(regex)
-    if (matcher) {
-        let replacer = url.replace(regex, '?single=1')
-        window.location.href = replacer
-    }
+async function urlMatcher() {
+	const url = window.location.href
+	
+	detikRedirect(url)
+	kompasRedirect(url)
+	tribunnewsRedirect(url)
+	merdekaRedirect(url)
+	suaraRedirect(url)
+	sindonewsRedirect(url)
+	inewsRedirect(url)
+	gridRedirect(url)
 }
 
 
-function kompasRedirect() {
-    let url = window.location.href
-    let regex = /(?<=^.+\.kompas.com\/(\w*(\/|))read\/\d+\/\d+\/\d+\/\d+\/.+)(\?page=\d|\/|(?<!\?page=all.*))$/
-    let matcher = url.match(regex)
-    if (matcher) {
-        let replacer = url.replace(regex, '?page=all')
-        window.location.href = replacer
-    }
+function redirectHelper(url, regex, replacer) {
+	let matcher = url.match(regex)
+	if (matcher) {
+		let newUrl = url.replace(regex, replacer)
+		window.location.href = newUrl
+		return
+	}
 }
 
 
-detikRedirect() 
-kompasRedirect()
+function detikRedirect(url) { 
+	new Promise (() => { 
+		//let regex = /(?<=^.+\.detik\.com\/.+\/.+\/.+)(\?(tag_from|(?!single).*)|(?<=[a-zA-Z])|\/\d?)$/
+		let regex = /(?<=^.+\.detik\.com\/.+\/.+\/.+)((?<!\?single=\d)|\?tag_from.*|\/\d*)$/
+		redirectHelper(url, regex, '?single=1')
+		console.log('detik')
+	})
+}
+
+
+function kompasRedirect(url) { 
+	new Promise (() => {
+		let regex = /(?<=^.+\.kompas.com\/(\w*(\/|))read\/\d+\/\d+\/\d+\/\d+\/.+)((?<!\?page=all)|\?page=\d|\/)$/
+		redirectHelper(url, regex, '?page=all')
+		console.log('kompas')
+	})
+}
+
+
+function tribunnewsRedirect(url) { 
+	new Promise (() => {
+		let regex = /(?<=^.+.tribunnews.com\/\w+\/\d+\/\d+\/\d+\/.+)((?<!\?page=all)|\?page=\d|\/)$/
+		redirectHelper(url, regex, '?page=all')
+		console.log('tribun')
+	})
+}
+
+
+function merdekaRedirect(url) {
+	new Promise (() => {
+		let regex = /(?<=^.+\.merdeka\.com\/\w+\/.+\.html)(\?page=\d|\/|)$/
+		redirectHelper(url, regex, '?page=all')
+		console.log('merdeka')
+	})
+}
+
+
+function suaraRedirect(url) {
+	new Promise (() => {
+		let regex = /(?<=^.+\.(suara|matamata)\.com\/\w+\/\d+\/\d+\/\d+\/\d+\/.+)((?<!\?page=all)|\?page=\d|\/)$/
+		redirectHelper(url, regex, '?page=all')
+		console.log('suara')
+	})
+}
+
+
+function sindonewsRedirect(url) {
+	new Promise (() => {
+		let regex = /(?<=^.+\.sindonews\.com\/read\/\d+\/\d+\/.+)((?<!\?showpage=all)|\/\d*)$/
+		redirectHelper(url, regex, '?showpage=all')
+		console.log('sindo')
+	})
+}
+
+
+function inewsRedirect(url) {
+	new Promise (() => {
+		let regex = /(?<=^.+\.inews\.id\/\w+\/\w+\/.+)((?<!\/all)|\/\d*)$/
+		redirectHelper(url, regex, '/all')
+		console.log('inews')
+	})
+}
+
+
+function gridRedirect(url) {
+	new Promise (() => {
+		let regex = /(?<=^.+\.(grid\.id|(motorplus-online|gridoto|bolasport)\.com)\/read\/\w+\/.+)((?<!\?page=all)|\?page=\d|\/)$/
+		let matcher = url.match(regex)
+		redirectHelper(url, regex, '?page=all')
+		console.log('grid')
+	})
+}
+
+
+urlMatcher()
